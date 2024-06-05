7ee4f133981bc4b617f2ba6248ae4e2ecb91c10f
markjerichomanilag <mark@betii.ai>
2024-06-05 15:04:29 -0400

    removed unused imports

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,6 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
-import { useSearchParams } from 'next/navigation';
-import { sql } from '@vercel/postgres';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
