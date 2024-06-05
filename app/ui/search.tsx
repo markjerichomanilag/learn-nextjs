5192b173d2a3eb664348b3ce8840f4668c99844a
markjerichomanilag <mark@betii.ai>
2024-06-05 11:58:38 -0400

    handle change name to handle search and apply useDebounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,13 +3,14 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams } from 'next/navigation';
 import { useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchparams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleChange = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchparams);
     if (term) {
       params.set('query', term);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}/?${params.toString()}`);
-  };
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -28,7 +29,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => handleChange(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
         defaultValue={searchparams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
