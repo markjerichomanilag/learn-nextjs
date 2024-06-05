0d273acfe421260ba22a27e2903d586127a0e8cd
markjerichomanilag <mark@betii.ai>
2024-06-05 17:36:26 -0400

    render pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -11,9 +11,13 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = searchParams.get('page') || 1;
+  let currentPage = searchParams.get('page') || 1;
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  if (typeof currentPage === 'string') {
+    currentPage = parseInt(currentPage);
+  }
+
+  const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = (pageNumber: Number | string) => {
     const params = new URLSearchParams(searchParams);
@@ -25,7 +29,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -58,7 +62,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
