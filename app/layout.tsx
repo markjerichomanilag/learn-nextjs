e3284f1b6d82f5ae56a73fd24ce11133abc30aae
markjerichomanilag <mark@betii.ai>
2024-06-06 09:56:08 -0400

    add metadata to app layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js course Dashboard, built with App Router.',
+  metadataBase: new URL('https://learn-nextjs-ten-gold.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
